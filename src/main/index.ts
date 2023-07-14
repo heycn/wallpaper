11061479d1ce24f8996d03d365b3e9ddf2770443
heycn <heycn@foxmail.com>
2023-07-15 01:16:41 +0800

    禁止缩放

--- src/main/index.ts
+++ src/main/index.ts
@@ -11,6 +11,7 @@ function createWindow(): void {
     height: 380,
     show: false,
     frame: false,
+    resizable: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
