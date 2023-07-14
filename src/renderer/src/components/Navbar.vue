97c1d5de4a3e8281977ef6f242eb2661fe8b09af
heycn <heycn@foxmail.com>
2023-07-14 22:58:47 +0800

    使用 icon-park

--- src/renderer/src/components/Navbar.vue
+++ src/renderer/src/components/Navbar.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+import { Pic, Setting } from '@icon-park/vue-next'
+</script>
+
 <template>
-  <nav>
+  <nav class="p-2 text-sm opacity-80 flex justify-between items-center">
+    <span class="w-12"></span>
     <h1>桌面壁纸</h1>
+    <span class="flex items-center justify-center gap-2">
+      <Pic theme="outline" size="20" fill="#333" />
+      <Setting theme="outline" size="20" fill="#333" />
+    </span>
   </nav>
 </template>
