cf736f68a6144ea2210e6f4e8e020f7f805a8d31
heycn <heycn@foxmail.com>
2023-07-15 20:05:33 +0800

    图片可以下载了

--- client/src/main/ipc.ts
+++ client/src/main/ipc.ts
@@ -1,6 +1,17 @@
 import { IpcMainEvent } from "electron";
 import { ipcMain } from "electron";
+import { createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream';
+import { promisify } from 'node:util'
+import fetch from 'node-fetch';
+import { resolve } from "node:path";
 
-ipcMain.on('setWallpaper', (_event: IpcMainEvent, url: string) => {
-  console.log('ipcMain', url)
+
+ipcMain.on('setWallpaper', async (_event: IpcMainEvent, url: string) => {
+  const localFile = resolve(__dirname, '../../wallpaper', url.split('/').pop()!)
+  console.log(localFile)
+  const streamPipeline = promisify(pipeline)
+  const response = await fetch(url)
+  if (!response.ok) throw new Error(`unexpected response ${response.statusText}`)
+  await streamPipeline(response.body!, createWriteStream(localFile))
 })
