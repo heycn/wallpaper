e7eab54460663444f07b3e780e3601e5e24c088f
heycn <heycn@foxmail.com>
2023-07-15 20:13:51 +0800

    完成失败时的错误提示

--- client/src/main/ipc.ts
+++ client/src/main/ipc.ts
@@ -1,4 +1,4 @@
-import { IpcMainEvent } from "electron";
+import { IpcMainEvent, dialog } from "electron";
 import { ipcMain } from "electron";
 import { createWriteStream } from 'node:fs';
 import { pipeline } from 'node:stream';
@@ -12,6 +12,9 @@ ipcMain.on('setWallpaper', async (_event: IpcMainEvent, url: string) => {
   console.log(localFile)
   const streamPipeline = promisify(pipeline)
   const response = await fetch(url)
-  if (!response.ok) throw new Error(`unexpected response ${response.statusText}`)
+  if (!response.ok) {
+    dialog.showErrorBox('糟糕！', `下载壁纸失败，请稍后再试\nstatus: ${response.statusText}`)
+    throw new Error(`unexpected response ${response.statusText}`)
+  }
   await streamPipeline(response.body!, createWriteStream(localFile))
 })
