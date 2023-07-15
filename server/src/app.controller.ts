0002b3ddf328ca4dfc7b1ffda763a184b10b5302
heycn <heycn@foxmail.com>
2023-07-15 16:26:20 +0800

    后端可以随机获取图片了

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Res } from '@nestjs/common';
+// import { AppService } from './app.service';
+import { readFileSync, readdirSync } from 'fs';
+import { resolve } from 'path';
+import { random } from 'lodash';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
-
+  getImage(@Res() res: Response) {
+    const files = readdirSync(resolve(__dirname, '../wallpaper'))
+    const images = files.filter(file => file.endsWith('.jpg') || file.endsWith('.png') || file.endsWith('.jpeg'))
+    const imageName = images[random(images.length - 1)]
+    const file = readFileSync(resolve(__dirname, '../wallpaper', imageName))
+    res.type('image/jpeg')
+    return res.send(file)
   }
 }
