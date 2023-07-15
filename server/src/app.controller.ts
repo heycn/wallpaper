f821b119f22b87560750bd4982ed6844a8520965
heycn <heycn@foxmail.com>
2023-07-15 14:46:15 +0800

    后端可以获取静态资源了

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -8,5 +8,6 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+
   }
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets('wallpaper', { prefix: '/wallpaper' });
   await app.listen(3000);
 }
 bootstrap();
