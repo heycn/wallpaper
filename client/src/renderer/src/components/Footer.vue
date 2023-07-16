7bdeec8740080965d66d1ec07d3bd90295daad07
heycn <heycn@foxmail.com>
2023-07-16 19:10:45 +0800

    完成缓存目录设置

--- client/src/renderer/src/components/Footer.vue
+++ client/src/renderer/src/components/Footer.vue
@@ -5,11 +5,11 @@ import { Github, Mail } from '@icon-park/vue-next'
 <template>
   <footer class="mb-2 opacity-70 text-sm flex justify-center gap-3 font-sans">
     联系我:
-    <a href="https://github.com/heycn" target="_blank" rel="noopener noreferrer" class="flex gap-1 items-center">
+    <a href="https://github.com/heycn" target="_blank" rel="noopener noreferrer" class="flex gap-1 items-center underline">
       <Github theme="outline" size="18" />
       <span>heycn</span>
     </a>
-    <a href="mailto:heycn@foxmail.com" title="go to my github!" target="_blank" class="flex gap-1 items-center">
+    <a href="mailto:heycn@foxmail.com" title="go to my github!" target="_blank" class="flex gap-1 items-center underline">
       <Mail theme="outline" size="18" />
       <span>heycn@foxmail.com</span>
     </a>
--- client/src/renderer/src/views/Settting.vue
+++ client/src/renderer/src/views/Settting.vue
@@ -1,6 +1,18 @@
 <script setup lang="ts">
+
 </script>
 
 <template>
-  settting...
+  <section class="m-4">
+    <h2>壁纸缓存目录</h2>
+    <div class="ml-2">
+      <p class="mt-2 text-sm opacity-60 font-thin">目前在：</p>
+      <div class="flex items-center">
+        <button class="underline hover:opacity-60 duration-300 font-thin text-sm">/User/chennan/sdsd</button>
+        <button class="text-blue-500 hover:text-blue-400 duration-300 ml-4">
+          更改
+        </button>
+      </div>
+    </div>
+  </section>
 </template>
